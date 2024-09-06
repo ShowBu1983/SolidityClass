d791e20a4a0c0b4b5c4fb4e8c7e2478f25e380c9
ShowBu1983 <77724608@qq.com>
2024-09-06 14:09:25 +0800

    +add some code comments

--- contract/MultiSigWallet.sol
+++ contract/MultiSigWallet.sol
@@ -1,9 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+/**
+多签钱包DEMO
+*/
 contract MultiSigWallet{
-    address[] public owners;
+    address[] public owners; //状态变量：声明在函数外，默认存储在区块链上（不需要加上数据位置）
     mapping(address=>bool) public isOwner;
+
     //定义 需要多少人以上完成一笔交易
     uint256 public required;
 
@@ -17,6 +21,10 @@ contract MultiSigWallet{
     Transaction[] public transactions;
     mapping(uint256 => mapping(address=>bool)) public approved;
 
+/**
+*以太坊的事件（Event）机制是一种在智能合约中定义和触发事件的方式，用于实现合约与外部世界的通信和提供交易的可追溯性。
+*事件机制可以让智能合约在特定条件满足时触发事件，并将相关信息记录在以太坊区块链上的日志中。
+*/
     //往多钱包里存钱
     event Deposit(address indexed sender, uint256 amount);
     //提交交易txID = transaction ID 交易的序号
@@ -28,7 +36,7 @@ contract MultiSigWallet{
     //执行交易
     event Execute(uint256 indexed txId);
 
-    //检测MSG sender 是否时钱包所有人之一
+    //检测MSG sender 是否是钱包所有人之一
     modifier onlyOwner(){
         require(isOwner[msg.sender], "not owner");
         _;
@@ -40,7 +48,7 @@ contract MultiSigWallet{
         _;
     }
 
-    //检查该交易是否已经被调用过
+    //检查该交易是否已经被批准过
     modifier notApproved(uint256 _txId){
         require(approved[_txId][msg.sender], "tx already approved");
         _;
@@ -56,8 +64,8 @@ contract MultiSigWallet{
         require(_owners.length > 0, "owner required");
         require(_required > 0 && _required <= owners.length, "ivalid required number of owners");
 
-        for(uint256 index = 0; index < _owners.length; index ++){
-            address owner = _owners[index];
+        for(uint256 i = 0; i < _owners.length; i ++){
+            address owner = _owners[i];
             //保证地址不为0x000。。.000
             require(owner != address(0), "invalid owner");
             //保证没有重复的地址
@@ -113,6 +121,8 @@ contract MultiSigWallet{
         require(getApproveCount(_txId) >= required, "not enough people agree this transaction");
         Transaction storage transaction = transactions[_txId];
         transaction.executed = true;
+        //目标合约地址.call{value:发送ETH数额， gas：gas数额}(二进制编码)
+        //abi.encodeWithSignature("函数签名", 逗号分隔具体参数)
         // (bool suc, ) = transaction.to.call{value:transaction.value}(transaction.data);
         // require(suc, "tx failed");
         payable(transaction.to).transfer(transaction.value);
